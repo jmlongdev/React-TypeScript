8be1a95861985637ae0e81e8d3bc5e6e9b793c27
jmlongdev <maxsoccerssport@hotmail.com>
2022-08-11 16:41:29 -0400

    learning typescript

---
+++ typescript-demo/basics.ts
@@ -0,0 +1,17 @@
+// Primitives - numbers, strings, booleans
+// More complex types: arrays, objects
+// Function types, parameters
+
+//Primitives - core types below
+
+let age: number = 24;
+// or
+age = 12;
+
+let userName: string = "Billybob";
+// or
+userName = "Bobbybob";
+
+let isStudent: boolean = true;
+
+// dont typically set null and undefined //
