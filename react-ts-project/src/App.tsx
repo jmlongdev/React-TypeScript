0c44e4799d3639b596adda0b4b690d1c961641ad
jmlongdev <maxsoccerssport@hotmail.com>
2022-08-15 15:15:55 -0400

    typescript

--- react-ts-project/src/App.tsx
+++ react-ts-project/src/App.tsx
@@ -1,30 +1,14 @@
-import { useState } from "react";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
+
 import NewTodo from "./components/NewTodo";
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (text: string) => {
-    const newTodo = new Todo(text);
-
-    setTodos((currentTodos) => {
-      return currentTodos.concat(newTodo);
-    });
-  };
-
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== todoId);
-    });
-  };
-
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
--- react-ts-project/src/components/NewTodo.tsx
+++ react-ts-project/src/components/NewTodo.tsx
@@ -1,7 +1,11 @@
-import React, { useRef } from "react";
-import { prototype } from "stream";
+import React, { useRef, useContext } from "react";
+
+import { TodosContext } from "../store/todos-context";
 import classes from "./NewTodo.module.css";
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+
+const NewTodo: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   const todoTextinput = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -13,7 +17,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
       return;
     }
 
-    props.onAddTodo(enteredText);
+    todosCtx.addTodo(enteredText);
   };
 
   return (
